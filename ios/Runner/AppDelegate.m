cdcf37191d2beb5ba628455aa3783090a0b2ec1b
devacc <affar2k@gmail.com>
2020-12-10 12:31:59 +0300

    changed some things

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,6 +7,8 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  // @todo: the google maps code
+  [GMSServices provideAPIKey:@"AIzaSyCSs-5RGgHaBrUBVlTxdYmrOEHhO9BbQpM"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 

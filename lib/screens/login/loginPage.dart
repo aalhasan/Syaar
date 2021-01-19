dac77ee3549568705e0148bf31b2f80a1d41364a
devacc <affar2k@gmail.com>
2021-01-19 12:23:45 +0300

    new stuff

---
+++ lib/screens/login/loginPage.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class loginPage extends StatelessWidget{
+
+  loginPage(Key key, String title):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+  }
+
+
+
+}
